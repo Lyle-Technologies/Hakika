d0ab97afe3901a0e3f1e630b09112a47f481927a
Steven Otieno <steveotianga8991@gmail.com>
2023-04-28 15:37:54 +0300

    Styled the images categories section from the API

--- src/Components/CategoryImages.js
+++ src/Components/CategoryImages.js
@@ -2,14 +2,16 @@ import CategoryImageContainer from "./CategoryImageContainer";
 import useFetch from "./useFetch";
 
 const CategoryImages = () => {
-  const { data: category } = useFetch("http://localhost:3004/categories");
+  const { data: category } = useFetch(
+    "https://hakika-online-store-api.onrender.com/api/categories"
+  );
 
   return (
     <div className={"d-flex imageScrollable overflow-scroll"}>
       {category.map((image) => (
-        <div key={image.id} className={"col-4"}>
+        <div key={image._id} className={"col-4"}>
           <CategoryImageContainer
-            imageLink={image.image}
+            imageLink={image.categoryImageLink}
             categoryTitle={image.name}
           />
         </div>
--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,23 +1,29 @@
 import { SlOptions } from "react-icons/sl";
 import { FcLikePlaceholder } from "react-icons/fc";
 
-const ProductCard = ({ imageLink, productTitle, productPrice, handleNavigate}) => {
+const ProductCard = ({
+  imageLink,
+  productTitle,
+  productPrice,
+  handleNavigate,
+}) => {
   return (
     <div className={"productCard mb-5"} onClick={handleNavigate}>
       <div className={"d-flex justify-content-between p-2"}>
         <SlOptions />
         <FcLikePlaceholder />
       </div>
-      <img
-        style={{ width: "100%", height: "170px", maxWidth: "100%" }}
-        className={"img-fluid"}
-        src={imageLink}
-        alt={"sample"}
-      />
+      <div>
+        <img
+          className={"img-fluid"}
+          src={imageLink}
+          alt={"sample"}
+        />
 
-      <div className={"p-2"}>
-        <p className={"boldweight"}>{productTitle}</p>
-        <p>Ksh {productPrice}</p>
+        <div className={"p-2"}>
+          <p className={"boldweight"}>{productTitle}</p>
+          <p>Ksh {productPrice}</p>
+        </div>
       </div>
     </div>
   );
